30353068c3fedba95b2a97229cea53a644394f0b
Morningstarpayments <31256375+Morningstarpayments@users.noreply.github.com>
2017-09-25 17:31:37 -0600

    Update coins.py

--- lib/coins.py
+++ lib/coins.py
@@ -1053,11 +1053,28 @@ class Morningstar(Coin):
     WIF_BYTE = bytes.fromhex("B0")
     GENESIS_HASH = ('7f06270eada62ade81aa19b05a73b8cb'
                     '2d9ad9ea7720a5277bb1de8b38a32dee')
-    DESERIALIZER = DeserializerSegWit
-    TX_COUNT = 10680
-    TX_COUNT_HEIGHT = 10680
-    TX_PER_BLOCK = 2
-    RPC_PORT = 29086
-    REORG_LIMIT = 1000
+    DESERIALIZER = DeserializerTxTime
+    DAEMON = daemon.LegacyRPCDaemon
+    TX_COUNT = 10000
+    TX_COUNT_HEIGHT = 10000
+    TX_PER_BLOCK = 3
+    IRC_PREFIX = "E_"
+    IRC_CHANNEL = "#electrum-mrng"
+    RPC_PORT = 15715
+    REORG_LIMIT = 5000
+    HEADER_HASH = None
+
+    @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        if cls.HEADER_HASH is None:
+            import scrypt
+            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
+
+        version, = struct.unpack('<I', header[:4])
+        if version > 6:
+            return super().header_hash(header)
+        else:
+            return cls.HEADER_HASH(header)
 
     
