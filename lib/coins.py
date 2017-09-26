dd646b80896908bf59403490b07321129fe412ea
Morningstarpayments <31256375+Morningstarpayments@users.noreply.github.com>
2017-09-26 13:11:47 -0600

    Update coins.py

--- lib/coins.py
+++ lib/coins.py
@@ -1063,6 +1063,19 @@ class Morningstar(Coin):
     IRC_CHANNEL = "#electrum-mrng"
     RPC_PORT = 29086
     REORG_LIMIT = 5000
+    
+     @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        if cls.HEADER_HASH is None:
+            import scrypt
+            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
+
+        version, = struct.unpack('<I', header[:4])
+        if version > 6:
+            return super().header_hash(header)
+        else:
+            return cls.HEADER_HASH(header)
    
 
     
